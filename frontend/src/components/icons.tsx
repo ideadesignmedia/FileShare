a656e47e1aba1fba5c2c7550b36035dc1ae4ab5e
ideadesignmedia <60307610+ideadesignmedia@users.noreply.github.com>
2025-03-24 11:49:47 -0700

    Updates

--- frontend/src/components/icons.tsx
+++ frontend/src/components/icons.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default {
-    down: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
-    <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7"/>
+    down: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7"/>
   </svg>
 }
