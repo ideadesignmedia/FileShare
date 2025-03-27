daad6ddde3c891763772ca336ece2234fa0c60ff
ideadesignmedia <60307610+ideadesignmedia@users.noreply.github.com>
2025-03-27 16:10:59 -0700

    Update post-build.js

--- frontend/post-build.js
+++ frontend/post-build.js
@@ -12,3 +12,26 @@ if (fs.existsSync(cordovaDir)) {
 }
 execSync(`cp -R ${dist} ${electronDir}`)
 execSync(`cp -R ${dist} ${cordovaDir}`)
+
+//replace index.html in cordova
+fs.readFile(path.join(dist, 'index.html'), 'utf8', (readErr, data) => {
+    if (readErr) {
+      console.error('Error reading file:', readErr);
+      process.exit(1);
+    }
+  
+    // Define the script tag to inject
+    const scriptTag = '    <script src="cordova.js"></script>\n';
+    // Insert the script tag before the closing </head> tag
+    const modifiedHtml = data.replace('<head>', '<head>\n' + scriptTag);
+  
+    // Write the modified HTML to the new location
+    fs.writeFile(path.join(cordovaDir, 'index.html'), modifiedHtml, 'utf8', (writeErr) => {
+      if (writeErr) {
+        console.error('Error writing file:', writeErr);
+      }
+      console.log('File written successfully to', outputPath);
+    });
+  });
+
+  
