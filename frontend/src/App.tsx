bd23b780ec2bc0d12d9d6c5e4ab378107377fb3e
ideadesignmedia <60307610+ideadesignmedia@users.noreply.github.com>
2025-03-27 12:34:34 -0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,14 +21,14 @@ function App() {
         <div className="flex flex-col grow items-center justify-center gap-3 w-full p-4">
           <h1>Login</h1>
           <p className="">Use any username and password and hit submit to get started.<br/><br/>Use the same username and password on your other devices to connect and share files using a peer to peer connection.</p>
-          <form className="flex flex-col items-center justify-start gap-1" onSubmit={e => {
+          <form className="flex flex-col items-center justify-start gap-2 w-2/3" onSubmit={e => {
             e.preventDefault()
             dispatch({ type: 'set-credentials', credentials })
             setTimeout(() => open(), 100)
           }}>
-            <input type="text" placeholder="Username" value={credentials.username} onChange={e => setCredentials({ ...credentials, username: e.target.value })} />
-            <input type="password" placeholder="Password" value={credentials.password} onChange={e => setCredentials({ ...credentials, password: e.target.value })} />
-            <Button type="submit" size="lg">Login</Button>
+            <input className="w-full border-2 border-blue-600 focus:border-blue-400" type="text" placeholder="Username" value={credentials.username} onChange={e => setCredentials({ ...credentials, username: e.target.value })} />
+            <input className="w-full border-2 border-blue-600 focus:border-blue-400" type="password" placeholder="Password" value={credentials.password} onChange={e => setCredentials({ ...credentials, password: e.target.value })} />
+            <Button className="w-full" type="submit" size="lg">Login</Button>
           </form>
           <p className="text-center text-sm text-red-600 opacity-80">If you are having errors signing in try another username.</p>
         </div>
